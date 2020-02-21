ad1db58b21b97a0b395b7f3a34a62601185ef61e
iamsirid <iamsirid@gmail.com>
2020-02-21 09:08:14 +0700

    add announcement

--- portfolio-source/src/App.js
+++ portfolio-source/src/App.js
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
-import "./App.scss";
+import React, { useState } from 'react';
+import './App.scss';
 
-import Left from "./components/Left";
-import Right from "./components/Right";
-import Detail from "./components/projects/Detail";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Left from './components/Left';
+import Right from './components/Right';
+import Detail from './components/projects/Detail';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 function App() {
   // const [project, setProject] = useState("");
   // const showDetail = project => {
@@ -19,9 +19,15 @@ function App() {
   //     <Right showDetail={showDetail} />
   //   </>
   // );
-
   return (
     <Router basename="/portfolio">
+      <div className="announcement">
+        This version of portfolio was used for job interviews in late 2019 and
+        has not been updated since then. <br /> If you're interesting or want
+        more information please contact me directly via{' '}
+        <a href="https://www.linkedin.com/in/sirid-rodsuteewat">Linkedin</a>
+      </div>
+
       <div className="main">
         <Switch>
           <Route exact path="/">
